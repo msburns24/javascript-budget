ddb484f80709782acf22f1d456c92db8bb9fa487
Matt <msb@ameritech.net>
2023-01-08 07:02:20 -0500

    Add Jan 8 Transactions, modify categories

--- main.js
+++ main.js
@@ -2,30 +2,7 @@ import { Transactions } from "./modules/transactions.js";
 import { DocumentHelper } from "./modules/document-helper.js";
 import { CSVDataFrame } from "./modules/csv-data-frame.js";
 
-function main() {
-  
-// let trans = {
-//   "Date":["12/31/2022","12/31/2022","12/31/2022","12/31/2022","12/31/2022","12/31/2022","12/30/2022","12/30/2022","12/28/2022","12/28/2022","12/28/2022","12/27/2022","12/27/2022","12/26/2022","12/26/2022","12/26/2022","12/26/2022", "12/25/2022"],
-//   "Vendor":["Raleigh Beer Garden","BP","Jersey Mike's","Target","Bulbox","Dank Burrito","Harris Teeter","Kohl's","Wake County ABC","Five Guys","Harris Teeter","HelloFresh","Amazon","Amazon","Amazon","Amazon","Amazon", "Spectrum"],
-//   "Category":["Restaurants","Auto","Restaurants","Home","Restaurants","Restaurants","Groceries","Home","Alcohol","Restaurants","Groceries","Groceries","Home","Home","Home","Home","Home", "Internet"],
-//   "Amount":[206.59,42.43,23.12,47.29,22.6,18.61,91.31,16.08,111.07,39.84,309,73.48,73.15,5.35,23.58,34.53,134.87,68.55]
-// };
-
-
-
-
-// WHere I left off:
-//
-// csv-data-frame module uses a require() function, which works server-side but not client-side.
-// Some options to implement this, like browserify:
-// https://github.com/browserify/browserify
-
-
-
-
-
-
-const filename = "./csv/2023.01.02_transactions.csv";
+const filename = "./csv/2023.01.08_transactions.csv";
 const rawData = new CSVDataFrame(filename);
 const trans = rawData.asArrays;
 
@@ -36,7 +13,3 @@ docHelperObj.addHeaderDates();
 docHelperObj.addTotalSpends();
 docHelperObj.addAllTopCategories();
 docHelperObj.createTransPages();
-
-}
-
-main();
